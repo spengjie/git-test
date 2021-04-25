2945b6c79937744e37e0c8edcb07e4be57366007
spengjie <spengjie@sina.com>
2021-04-25 15:21:19 +0800

    add access log

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import current_app, Flask
 
 
 app = Flask(__name__)
@@ -6,4 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    current_app.logger.info('Hello, World!')
     return 'Hello, World!'
+
+
+@app.route('/error')
+def error():
+    current_app.logger.error('Error!')
+    return 'Error!'
